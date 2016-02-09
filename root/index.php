b69ed3a76c0f34495df7862a7b651f4a765fd1fe
Negatu <nnegatu@gmail.com>
2016-02-08 22:27:01 -0500

    First commit.. Main folders and init files

---
+++ root/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title> Social Network Demo</title>
+	<style type="text/css">
+	body { margin:0px; }
+	#pageTop {
+		background: url(style/headerSliver.png) repeat-x;
+		height: 90px;
+	}
+	#pageTop > #pageTopWrap {
+		width: 1000px;
+		margin: 0px auto;
+		height: 90px;
+	}
+	</style>
+</head>
+
+<body>
+	<div id="pageTop"> 
+		<div id="pageTopWrap"> </div>
+	</div>
+	<div id="pageMiddle"> </div>
+	<div id="pageBottom"> </div>
+</body>
+
+</html>
